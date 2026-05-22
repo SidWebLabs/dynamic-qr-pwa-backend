3cd15ead24bd8f8a105e7261592f65b4edb40e59
siddhesh-kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2026-05-22 21:31:23 +0530

    feat: enhance server and app setup with improved error handling and response messages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
+// const routesV1 = require("./routes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+	cors({
+		origin: "*",
+		exposedHeaders: ["Content-Disposition"],
+	})
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+// app.use("/api/", routesV1);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to QR Pay Generator Backend");
+	res.status(200).json({
+		message: "Dynamic QR Backend Running",
+	});
+});
+
+app.use((req, res) => {
+	return res.status(404).json({
+		success: false,
+		message: "Route Not Found",
+	});
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,33 @@
 require("dotenv").config();
 
+const http = require("http");
+
 const app = require("./app");
 const sequelize = require("./config/database");
 
 const PORT = process.env.PORT || 5000;
 
+const server = http.createServer(app);
+
 sequelize
     .authenticate()
-    .then(() => {
+    .then(async () => {
         console.log("Database Connected");
 
-        app.listen(PORT, () => {
+        await sequelize.sync({ alter: true });
+
+        server.listen(PORT, () => {
             console.log(`Server running on ${PORT}`);
         });
     })
     .catch((err) => {
-        console.log(err);
+        console.log("Database Error:", err);
     });
+
+process.on("SIGINT", async () => {
+    console.log("Closing Server...");
+
+    await sequelize.close();
+
+    process.exit(0);
+});
